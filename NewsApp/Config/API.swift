b9ff0c9402e86428f60becc7475ff226ae973e0f
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-09-22 22:02:01 +0530

    Creating Model For the App

---
+++ NewsApp/Config/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  NewsApp
+//
+//  Created by Mahesh Prasad on 22/09/20.
+//  Copyright © 2020 CreatesApp. All rights reserved.
+//
+
+import Foundation
---
+++ NewsApp/Models/News.swift
@@ -0,0 +1,23 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by Mahesh Prasad on 22/09/20.
+//  Copyright © 2020 CreatesApp. All rights reserved.
+//
+
+import Foundation
+
+struct News: Decodable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let urlToImage: String?
+    let url: String
+}
+
+struct NewsEnvelope: Decodable {
+    let status: String
+    let totalResults: Int
+    let article:[News]
+}
